8e7e4693a0a1a2c7949888d5e5394e41d66bd86c
ysl2 <songli.yu@outlook.com>
2021-04-02 15:06:12 +0800

    2021-04-02 15:06:12

---
+++ git-upload.sh
@@ -0,0 +1,2 @@
+# git add . > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master -q > /dev/null
+git add . > /dev/null && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" > /dev/null && git push origin master
